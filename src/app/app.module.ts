66d76b3f02b73735ea1eda316a4fe09b4e563742
Iain Riley <iain.riley@gmail.com>
2017-08-26 13:24:46 +0800

    Broken Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ClarityModule } from 'clarity-angular';
+import { ChartModule } from 'angular-highcharts';
 
 import { AppComponent } from './app.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule.forRoot(),
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chart/chart.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { Chart } from 'angular-highcharts';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.css']
+})
+export class ChartComponent {
+
+  chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Linechart'
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        name: 'Line 1',
+        data: [1, 2, 3]
+      }]
+    });
+
+}
